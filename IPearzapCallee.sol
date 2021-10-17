cbdddde623a2477f77d4a3c972441d603eba9d60
Polymatic <84585696+polymaticproject@users.noreply.github.com>
2021-10-17 15:32:40 +0200

    Add files via upload

---
+++ IPearzapCallee.sol
@@ -0,0 +1,14 @@
+pragma solidity >=0.5.0;
+
+//  _ __   ___  __ _ _ __ ______ _ _ __  
+// | '_ \ / _ \/ _` | '__|_  / _` | '_ \ 
+// | |_) |  __/ (_| | |   / / (_| | |_) |
+// | .__/ \___|\__,_|_|  /___\__,_| .__/ 
+// | |                            | |    
+// |_|                            |_|    
+
+// https://pearzap.com/
+
+interface IPearzapCallee {
+    function pearzapCall(address sender, uint amount0, uint amount1, bytes calldata data) external;
+}
---
+++ IPearzapFactory.sol
@@ -0,0 +1,26 @@
+pragma solidity >=0.5.0;
+
+//  _ __   ___  __ _ _ __ ______ _ _ __  
+// | '_ \ / _ \/ _` | '__|_  / _` | '_ \ 
+// | |_) |  __/ (_| | |   / / (_| | |_) |
+// | .__/ \___|\__,_|_|  /___\__,_| .__/ 
+// | |                            | |    
+// |_|                            |_|    
+
+// https://pearzap.com/
+
+interface IPearzapFactory {
+    event PairCreated(address indexed token0, address indexed token1, address pair, uint);
+
+    function feeTo() external view returns (address);
+    function feeToSetter() external view returns (address);
+
+    function getPair(address tokenA, address tokenB) external view returns (address pair);
+    function allPairs(uint) external view returns (address pair);
+    function allPairsLength() external view returns (uint);
+
+    function createPair(address tokenA, address tokenB) external returns (address pair);
+
+    function setFeeTo(address) external;
+    function setFeeToSetter(address) external;
+}
